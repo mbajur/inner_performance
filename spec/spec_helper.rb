456b1d97f85f6407a9e6126986dfa44a00406dfd
mbajur <mbajur@gmail.com>
2024-11-24 20:11:00 +0100

    Set queue adapter to inline on test env

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -91,4 +91,8 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+
+  config.before(:suite) do
+    ActiveJob::Base.queue_adapter = :inline
+  end
 end
