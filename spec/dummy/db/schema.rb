353886495a88b35d5702987494e647542c1772f7
m.b. <mbajur@users.noreply.github.com>
2024-12-18 09:27:44 +0100

    Make database.yml actually use all the db engines on CI (#27)
    * Make database.yml actually use all the db engines on CI
    
    * Fix mysql database name
    
    * Remove default value from properties
    
    * Fix rubocop
    
    * Fix rubocop

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 20_241_124_111_458) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_18_082041) do
   create_table "inner_performance_events", force: :cascade do |t|
-    t.string("event")
-    t.string("name")
-    t.decimal("duration")
-    t.decimal("db_runtime")
-    t.datetime("created_at", null: false)
-    t.datetime("updated_at", null: false)
-    t.text("properties", default: "{}")
-    t.string("type")
+    t.string "event"
+    t.string "name"
+    t.decimal "duration"
+    t.decimal "db_runtime"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "properties"
+    t.string "type"
   end
 end
