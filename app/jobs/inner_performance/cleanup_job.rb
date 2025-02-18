bc7f061a935e0b9b5014e2e167e91ba7595f3855
m.b. <mbajur@users.noreply.github.com>
2025-02-18 16:53:31 +0100

    Use #delete in CleanupJob (#34)

--- app/jobs/inner_performance/cleanup_job.rb
+++ app/jobs/inner_performance/cleanup_job.rb
@@ -3,9 +3,14 @@
 module InnerPerformance
   class CleanupJob < ApplicationJob
     def perform
+      InnerPerformance::Trace
+        .joins(:event)
+        .where("inner_performance_events.created_at < ?", InnerPerformance.configuration.events_retention.ago)
+        .delete_all
+
       InnerPerformance::Event
         .where("created_at < ?", InnerPerformance.configuration.events_retention.ago)
-        .destroy_all
+        .delete_all
     end
   end
 end
