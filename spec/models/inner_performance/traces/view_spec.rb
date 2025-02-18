de1dab63fb460ca9721d07fc28d94e9b6cf39afc
mbajur <mbajur@gmail.com>
2025-02-18 14:30:56 +0100

    View and Db models specs

---
+++ spec/models/inner_performance/traces/view_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe InnerPerformance::Traces::View do
+  describe '.initialize_for_insert' do
+    subject { described_class.initialize_for_insert(trace: trace, event: event) }
+
+    let(:trace) do
+      {
+        name: 'render_template.action_view',
+        payload: { identifier: 'foo' },
+        duration: 123,
+        time: Time.current.to_i,
+      }
+    end
+
+    let(:event) { create(:event) }
+
+    it 'returns a hash with the correct keys and values' do
+      expect(subject).to(eq({
+        type: "InnerPerformance::Traces::View",
+        name: "render_template.action_view",
+        payload: { identifier: "foo" }.to_json,
+        duration: 123,
+        created_at: trace[:time],
+        event_id: event.id,
+      }))
+    end
+  end
+end
