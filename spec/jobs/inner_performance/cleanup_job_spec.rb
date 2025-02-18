2e418b8f102ac7cccdd47d13463cb5f947332b6b
mbajur <mbajur@gmail.com>
2025-02-18 16:51:02 +0100

    Use #delete in CleanupJob

--- spec/jobs/inner_performance/cleanup_job_spec.rb
+++ spec/jobs/inner_performance/cleanup_job_spec.rb
@@ -8,7 +8,16 @@ describe InnerPerformance::CleanupJob do
   let!(:old_event) { create(:event, created_at: 7.days.ago) }
   let!(:fresh_event) { create(:event, created_at: 6.days.ago) }
 
-  it "removes jobs older than InnerPerformance.configuration.events_retention" do
+  let!(:old_trace) { create(:trace, event: old_event, created_at: 7.days.ago) }
+  let!(:fresh_trace) { create(:trace, event: fresh_event, created_at: 6.days.ago) }
+
+  it "removes events older than InnerPerformance.configuration.events_retention" do
     expect { subject }.to(change(InnerPerformance::Event.all, :count).by(-1))
+    expect(InnerPerformance::Event.all).to(eq([fresh_event]))
+  end
+
+  it "removes traces older than InnerPerformance.configuration.events_retention" do
+    expect { subject }.to(change(InnerPerformance::Trace.all, :count).by(-1))
+    expect(InnerPerformance::Trace.all).to(eq([fresh_trace]))
   end
 end
