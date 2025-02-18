3a1084899c124d67d6087e14e2fdcf0251a7e135
mbajur <mbajur@gmail.com>
2025-02-18 13:46:03 +0100

    testfix

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_14_162622) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_14_162622) do
   create_table "inner_performance_events", force: :cascade do |t|
     t.string "event"
     t.string "name"
